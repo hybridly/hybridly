eeef40beec1732d6a0b5299700813fbd4c944851
Enzo Innocenzi <enzo@innocenzi.dev>
2022-02-26 16:08:02 +0100

    refactor(router): make header constants

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,4 +1,10 @@
 export const STORAGE_EXTERNAL_KEY = 'sleightful:external'
 export const SLEIGHTFUL_HEADER = 'x-sleightful'
 export const EXTERNAL_VISIT_HEADER = `${SLEIGHTFUL_HEADER}-external`
+export const PARTIAL_COMPONENT_HEADER = `${SLEIGHTFUL_HEADER}-partial-component`
+export const ONLY_DATA_HEADER = `${SLEIGHTFUL_HEADER}-only-data`
+export const EXCEPT_DATA_HEADER = `${SLEIGHTFUL_HEADER}-except-data`
+export const CONTEXT_HEADER = `${SLEIGHTFUL_HEADER}-context`
+export const VERSION_HEADER = `${SLEIGHTFUL_HEADER}-version`
+export const ERROR_BAG_HEADER = `${SLEIGHTFUL_HEADER}-error-bag`
 export const SCROLL_REGION_ATTRIBUTE = 'scroll-region'
