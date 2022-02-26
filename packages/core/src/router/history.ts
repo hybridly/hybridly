da7db3348f266ae127451aa6984538c698e893aa
Enzo Innocenzi <enzo@innocenzi.dev>
2022-02-26 03:34:09 +0100

    fix(core): handle basic history state visits

--- packages/core/src/router/history.ts
+++ packages/core/src/router/history.ts
@@ -2,6 +2,8 @@ import { navigate } from '..'
 import { debug } from '../utils'
 import { RouterContext } from './context'
 
+type SerializedContext = Omit<RouterContext, 'adapter'>
+
 /** Puts the given context into the history state. */
 export function setHistoryState(context: RouterContext, options: HistoryOptions = {}) {
 	if (!window?.history) {
@@ -12,9 +14,9 @@ export function setHistoryState(context: RouterContext, options: HistoryOptions 
 		? 'replaceState'
 		: 'pushState'
 
-	window.history[method](context, '', context.url)
+	debug.history('Setting history state:', { method, context })
 
-	debug.history('Set history state:', { method, context })
+	window.history[method](serializeContext(context), '', context.url)
 }
 
 /** Gets the current history state if it exists. */
@@ -51,6 +53,16 @@ export async function handleBackForwardVisit(context: RouterContext): Promise<vo
 	})
 
 	// restoreScrollPositions()
+}
+
+/** Serializes the context so it can be written to the history state. */
+export function serializeContext(context: RouterContext): SerializedContext {
+	return {
+		url: context.url,
+		version: context.version,
+		view: context.view,
+		dialog: context.dialog,
+	}
 }
 
 export interface HistoryOptions {
