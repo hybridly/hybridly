8daf54a6ada36d3bc53347ef20816b7cc982394f
Enzo Innocenzi <enzo@innocenzi.dev>
2022-02-26 02:54:33 +0100

    chore(core): support default value for `match`

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,16 +1,20 @@
 export function match<TValue extends string | number = string, TReturnValue = unknown>(
 	value: TValue,
-	lookup: Record<TValue, TReturnValue | ((...args: any[]) => TReturnValue)>,
+	lookup: Record<TValue | 'default', TReturnValue | ((...args: any[]) => TReturnValue)>,
 	...args: any[]
 ): TReturnValue | Promise<TReturnValue> {
-	if (value in lookup) {
-		const returnValue = lookup[value]
+	if (value in lookup || 'default' in lookup) {
+		const returnValue = value in lookup
+			? lookup[value]
+			: lookup.default
+
 		return typeof returnValue === 'function' ? returnValue(...args) : returnValue
 	}
 
 	const handlers = Object.keys(lookup)
 		.map((key) => `"${key}"`)
 		.join(', ')
+
 	const error = new Error(`Tried to handle "${value}" but there is no handler defined. Only defined handlers are: ${handlers}.`)
 
 	if (Error.captureStackTrace) {
