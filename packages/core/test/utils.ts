d27eb9e3531844e841010b632eefe9b6a2f21b8c
Enzo Innocenzi <enzo@innocenzi.dev>
2022-02-27 23:20:06 +0100

    test: use msw to intercept requests

--- packages/core/test/utils.ts
+++ packages/core/test/utils.ts
@@ -1,26 +1,31 @@
 import defu from 'defu'
+import { VisitPayload } from 'sleightful'
 import type { PartialDeep } from 'type-fest'
 import { createContext, RouterContext, RouterContextOptions } from '../src/router/context'
 
 export const noop = () => ({} as any)
 export const returnsArgs = (...args: any) => args
 
-export function makeRouterContextOptions(options: PartialDeep<RouterContextOptions> = {}): RouterContextOptions {
-	return defu({
-		payload: {
-			url: 'https://localhost',
-			version: 'abc123',
-			view: {
-				name: 'default.view',
-				properties: {},
-			},
+export function makeVisitPayload(payload: PartialDeep<VisitPayload> = {}): VisitPayload {
+	return defu(payload as VisitPayload, {
+		url: 'https://localhost',
+		version: 'abc123',
+		view: {
+			name: 'default.view',
+			properties: {},
 		},
+	})
+}
+
+export function makeRouterContextOptions(options: PartialDeep<RouterContextOptions> = {}): RouterContextOptions {
+	return defu(options as RouterContextOptions, {
+		payload: makeVisitPayload(),
 		adapter: {
 			resolveComponent: noop,
 			swapDialog: noop,
 			swapView: noop,
 		},
-	}, options)
+	})
 }
 
 export function fakeRouterContext(options: PartialDeep<RouterContextOptions> = {}): RouterContext {
