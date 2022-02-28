5803b94fb3db7ca06e27e5755d502fd18b26a10b
Enzo Innocenzi <enzo@innocenzi.dev>
2022-03-01 00:36:07 +0100

    feat: package subpackages in `sleightful`

--- packages/sleightful/src/vite.ts
+++ packages/sleightful/src/vite.ts
@@ -1,11 +1,2 @@
-import type { Plugin } from 'vite'
-import type { Options } from '@sleightful/vite'
-import sleightfully from '@sleightful/vite'
-
 export * from '@sleightful/vite'
-
-export default function(options?: Options): Plugin[] {
-	return [
-		sleightfully(options),
-	]
-}
+export { default } from '@sleightful/vite'
---
+++ packages/vite/src/layout.ts
@@ -0,0 +1,47 @@
+import path from 'node:path'
+import makeDebugger from 'debug'
+import { Plugin, normalizePath } from 'vite'
+import { Options } from './types'
+
+const PLUGIN_NAME = 'vite:sleightful:layout'
+const TEMPLATE_LAYOUT_REGEX = /<template +layout(?: *= *['"](?:(?:(\w+):)?(\w+))['"] *)?>/
+const debug = makeDebugger(PLUGIN_NAME)
+
+/**
+ * A basic Vite plugin that adds a <template layout="name"> syntax to Vite SFCs.
+ * It must be used before the Vue plugin.
+ */
+export default (options: Options = {}): Plugin => {
+	const base = options?.layout?.directory
+		? options?.layout?.directory
+		: path.resolve(process.cwd(), 'resources', 'views', 'layouts')
+	const layoutPath = (layoutName: string) => normalizePath(path.resolve(base, `${layoutName ?? 'default'}.vue`)).replaceAll('\\', '/')
+
+	debug('Registered layout path:', base)
+
+	return {
+		name: PLUGIN_NAME,
+		transform: (code: string, id: string) => {
+			if (!TEMPLATE_LAYOUT_REGEX.test(code)) {
+				return
+			}
+
+			debug(`Found Inertia layout call in ${id}`)
+
+			const isTypeScript = /lang=['"]ts['"]/.test(code)
+
+			return code.replace(TEMPLATE_LAYOUT_REGEX, (_, __, layoutName) => {
+				const importPath = layoutPath(layoutName)
+				debug(`Resolved "${layoutName}": ${importPath}`)
+
+				return `
+				<script${isTypeScript ? ' lang="ts"' : ''}>
+				import layout from '${importPath}'
+				export default { layout }
+				</script>
+				<template>
+			`
+			})
+		},
+	}
+}
