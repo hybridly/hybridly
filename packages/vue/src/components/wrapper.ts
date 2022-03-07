26f60452eae2af618bee805b7294ccaea80882aa
Enzo Innocenzi <enzo@innocenzi.dev>
2022-03-07 23:29:15 +0100

    fix(vue): ensure context and components are reactive

--- packages/vue/src/components/wrapper.ts
+++ packages/vue/src/components/wrapper.ts
@@ -1,4 +1,4 @@
-import { RouterContext } from '@sleightful/core'
+import { debug, RouterContext } from '@sleightful/core'
 import { ComponentOptions, defineComponent, h, PropType } from 'vue'
 import { state } from '../stores/state'
 
@@ -7,38 +7,67 @@ export const wrapper = defineComponent({
 	setup({ context, component }) {
 		if (typeof window !== 'undefined') {
 			state.setContext(context)
-			state.setComponent(component)
+			state.setView(component)
 
 			if (!context || !component) {
 				throw new Error('Sleightful was not properly initialized. The context or initial component is missing.')
 			}
 		}
 
-		return () => {
-			if (state.component.value) {
-				state.component.value.inheritAttrs = !!state.component.value.inheritAttrs
+		function renderLayout(child: any) {
+			debug.adapter('vue:render:layout', 'Rendering layout.')
 
-				const child = h(state.component.value, {
-					...state.context.value?.view?.properties,
-					key: state.key.value,
+			if (typeof state.view.value!.layout === 'function') {
+				return state.view.value!.layout(h, child)
+			}
+
+			if (Array.isArray(state.view.value!.layout)) {
+				return state.view.value!.layout
+					.concat(child)
+					.reverse()
+					.reduce((child, layout) => {
+						layout.inheritAttrs = !!layout.inheritAttrs
+						return h(layout, { ...state.context.value!.view.properties }, () => child)
+					})
+			}
+
+			return [
+				h(state.view.value!.value.layout, { ...state.context.value!.view.properties }, () => child),
+				renderDialog(),
+			]
+		}
+
+		function renderView() {
+			debug.adapter('vue:render:view', 'Rendering view.')
+			state.view.value!.inheritAttrs = !!state.view.value!.inheritAttrs
+
+			return h(state.view.value!, {
+				...state.context.value!.view.properties,
+				dialog: false,
+				key: state.viewKey.value,
+			})
+		}
+
+		function renderDialog() {
+			debug.adapter('vue:render:dialog', 'Rendering dialog.')
+
+			if (state.dialog.value) {
+				return h(state.dialog.value!, {
+					...state.dialog.value.properties,
+					key: state.dialogKey.value,
 				})
+			}
+		}
 
-				if (state.component.value.layout) {
-					if (typeof state.component.value.layout === 'function') {
-						return state.component.value.layout(h, child)
-					}
+		return () => {
+			if (state.view.value) {
+				const view = renderView()
 
-					return (Array.isArray(state.component.value.layout) ? state.component.value.layout : [state.component.value.layout])
-						.concat(child)
-						.reverse()
-						.reduce((child: any, layout: any) => {
-							layout.inheritAttrs = !!layout.inheritAttrs
-
-							return h(layout, { ...state.context.value?.view?.properties }, () => child)
-						})
+				if (state.view.value.layout) {
+					return renderLayout(view)
 				}
 
-				return child
+				return [view, renderDialog()]
 			}
 		}
 	},
--- packages/vue/src/stores/state.ts
+++ packages/vue/src/stores/state.ts
@@ -1,22 +1,35 @@
-import { RouterContext } from '@sleightful/core'
+import { debug, RouterContext } from '@sleightful/core'
 import { ComponentOptions, markRaw, Ref, ref, unref } from 'vue'
 
 type MaybeRef<T> = Ref<T> | T
 
 export const state = {
 	context: ref<RouterContext>(),
-	component: ref<ComponentOptions>(),
-	key: ref<number>(),
+	view: ref<ComponentOptions>(),
+	viewKey: ref<number>(),
+	dialog: ref<ComponentOptions>(),
+	dialogKey: ref<number>(),
 
-	setComponent(component: MaybeRef<ComponentOptions>) {
-		this.component.value = markRaw(unref(component))
+	setView(view: MaybeRef<ComponentOptions>) {
+		debug.adapter('vue:state:view', 'Setting view:', view)
+		this.view.value = markRaw(unref(view))
+	},
+
+	setDialog(dialog: MaybeRef<ComponentOptions>) {
+		debug.adapter('vue:state:dialog', 'Setting dialog:', dialog)
+		this.dialog.value = markRaw(unref(dialog))
 	},
 
 	setContext(context: MaybeRef<RouterContext>) {
+		debug.adapter('vue:state:context', 'Setting context:', context)
 		this.context.value = unref(context)
 	},
 
-	setKey(key: MaybeRef<number>) {
-		this.key.value = unref(key)
+	setViewKey(key: MaybeRef<number>) {
+		this.viewKey.value = unref(key)
+	},
+
+	setDialogKey(key: MaybeRef<number>) {
+		this.dialogKey.value = unref(key)
 	},
 }
