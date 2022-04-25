14aa074b6b989770b87bca732aa8fb6fca4d809d
Enzo Innocenzi <enzo@innocenzi.dev>
2022-04-25 23:30:43 +0200

    fix(form): properly save state on visit

--- packages/vue/src/composables/form.ts
+++ packages/vue/src/composables/form.ts
@@ -15,21 +15,28 @@ interface FormOptions<T extends Fields> extends Omit<VisitOptions, 'data'> {
 }
 
 export function useForm<T extends Fields = Fields>(options: FormOptions<T>) {
-	let recentlySuccessfulTimeoutId: any
 	const shouldRemember = options?.key !== false
 	const historyKey = options?.key as string ?? 'form:default'
-	const historyFields = shouldRemember ? router.history.get(historyKey) as T : undefined
+	const historyData = shouldRemember ? router.history.get(historyKey) as any : undefined
+	const timeoutIds = {
+		recentlyFailed: undefined as any,
+		recentlySuccessful: undefined as any,
+	}
 
-	/** Initial fields. */
+	/** Fields that were initially set up. */
 	const initial = readonly(clone(options.fields))
+	/** Fields as they were when loaded. */
+	const loaded = readonly(clone(historyData?.fields ?? options.fields))
 	/** Current fields. */
-	const fields = reactive(clone(historyFields ?? options.fields))
+	const fields = reactive<T>(clone(historyData?.fields ?? options.fields))
 	/** Validation errors for each field. */
-	const errors = ref<Record<string, string>>({})
+	const errors = ref<Record<string, any>>(historyData?.errors ?? {})
 	/** Whether the form is dirty. */
 	const isDirty = ref(false)
 	/** Whether the submission was recently successful. */
 	const recentlySuccessful = ref(false)
+	/** Whether the submission was recently failed. */
+	const recentlyFailed = ref(false)
 	/** Whether the submission is successful. */
 	const successful = ref(false)
 	/** Whether the submission is being processed. */
@@ -53,11 +60,13 @@ export function useForm<T extends Fields = Fields>(options: FormOptions<T>) {
 			...options,
 			...optionsOverrides,
 			data: options.transform?.(fields) ?? fields,
+			preserveState: optionsOverrides?.preserveState === undefined && options.method !== 'GET' ? true : optionsOverrides?.preserveState,
 			events: {
 				before: (visit) => {
 					successful.value = false
 					recentlySuccessful.value = false
-					clearTimeout(recentlySuccessfulTimeoutId)
+					clearTimeout(timeoutIds.recentlySuccessful)
+					clearTimeout(timeoutIds.recentlyFailed)
 					clearErrors()
 					return options.events?.before?.(visit)
 				},
@@ -66,15 +75,16 @@ export function useForm<T extends Fields = Fields>(options: FormOptions<T>) {
 					return options.events?.start?.(context)
 				},
 				error: (incoming) => {
-					clearErrors()
 					setErrors(incoming)
+					recentlyFailed.value = true
+					timeoutIds.recentlyFailed = setTimeout(() => recentlyFailed.value = false, options?.timeout ?? 5000)
 					return options.events?.error?.(incoming)
 				},
 				success: (payload) => {
 					reset()
 					successful.value = true
 					recentlySuccessful.value = true
-					recentlySuccessfulTimeoutId = setTimeout(() => recentlySuccessful.value = false, options?.timeout ?? 5000)
+					timeoutIds.recentlySuccessful = setTimeout(() => recentlySuccessful.value = false, options?.timeout ?? 5000)
 					return options.events?.success?.(payload)
 				},
 				after: (context) => {
@@ -96,7 +106,7 @@ export function useForm<T extends Fields = Fields>(options: FormOptions<T>) {
 	 * Sets current errors.
 	 */
 	function setErrors(incoming: Record<string, string>) {
-		errors.value = Object.assign({}, toRaw(incoming))
+		errors.value = incoming
 	}
 
 	/**
@@ -106,11 +116,14 @@ export function useForm<T extends Fields = Fields>(options: FormOptions<T>) {
 		router.abort()
 	}
 
-	watch(fields, () => {
-		isDirty.value = !isEqual(toRaw(initial), toRaw(fields))
+	watch([fields, processing, errors], () => {
+		isDirty.value = !isEqual(toRaw(loaded), toRaw(fields))
 
 		if (shouldRemember) {
-			router.history.remember(historyKey, toRaw(fields))
+			router.history.remember(historyKey, {
+				fields: toRaw(fields),
+				errors: toRaw(errors.value),
+			})
 		}
 	}, { deep: true, immediate: true })
 
@@ -118,15 +131,17 @@ export function useForm<T extends Fields = Fields>(options: FormOptions<T>) {
 		reset,
 		initial,
 		fields,
+		loaded,
 		submit,
-		hasErrors: computed(() => Object.values(errors.value).length > 0),
+		abort,
 		setErrors,
 		clearErrors,
-		abort,
+		hasErrors: computed(() => Object.values(errors.value).length > 0),
 		isDirty: readonly(isDirty),
 		errors: readonly(errors),
 		processing: readonly(processing),
 		successful: readonly(successful),
 		recentlySuccessful: readonly(recentlySuccessful),
+		recentlyFailed: readonly(recentlyFailed),
 	}
 }
