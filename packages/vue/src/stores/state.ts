423806ac16a088a6c35c35ab7c7b9b77c992baa6
Enzo Innocenzi <enzo@innocenzi.dev>
2022-06-28 18:55:00 +0200

    fix(vue): update properties on context changes

--- packages/vue/src/stores/state.ts
+++ packages/vue/src/stores/state.ts
@@ -1,37 +1,38 @@
 import { debug, RouterContext } from '@hybridly/core'
-import { ComponentOptions, markRaw, Ref, ref, unref } from 'vue'
+import { ComponentOptions, Ref, ref, shallowRef, triggerRef, unref } from 'vue'
 
 type MaybeRef<T> = Ref<T> | T
 
 export const state = {
 	context: ref<RouterContext>(),
-	view: ref<ComponentOptions>(),
+	view: shallowRef<ComponentOptions>(),
 	viewKey: ref<number>(),
-	dialog: ref<ComponentOptions>(),
+	dialog: shallowRef<ComponentOptions>(),
 	dialogKey: ref<number>(),
 
 	setView(view: MaybeRef<ComponentOptions>) {
 		debug.adapter('vue:state:view', 'Setting view:', view)
-		this.view.value = markRaw(unref(view))
+		state.view.value = view
 	},
 
 	setDialog(dialog: MaybeRef<ComponentOptions>) {
 		debug.adapter('vue:state:dialog', 'Setting dialog:', dialog)
-		this.dialog.value = markRaw(unref(dialog))
+		state.dialog.value = dialog
 	},
 
 	setContext(context: MaybeRef<RouterContext>) {
 		debug.adapter('vue:state:context', 'Setting context:', context)
-		this.context.value = unref(context)
+		state.context.value = unref(context)
+		triggerRef(state.context)
 	},
 
 	setViewKey(key: MaybeRef<number>) {
 		debug.adapter('vue:state:key', 'Setting view key:', key)
-		this.viewKey.value = unref(key)
+		state.viewKey.value = unref(key)
 	},
 
 	setDialogKey(key: MaybeRef<number>) {
 		debug.adapter('vue:state:key', 'Setting dialog key:', key)
-		this.dialogKey.value = unref(key)
+		state.dialogKey.value = unref(key)
 	},
 }
