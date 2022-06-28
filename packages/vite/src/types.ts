58fac2b598bc158c2b0a3bc7b3823239fef05858
Enzo Innocenzi <enzo@innocenzi.dev>
2022-06-28 18:35:14 +0200

    fix(vite): make layout plugin opt-out

--- packages/vite/src/types.ts
+++ packages/vite/src/types.ts
@@ -2,8 +2,10 @@ import { Options as LaravelOptions } from 'vite-plugin-laravel'
 
 export interface Options extends LaravelOptions {
 	/** Options for the layout plugin. */
-	layout?: {
-		/** The directory in which layouts are stored. */
-		directory?: string
-	}
+	layout?: false | LayoutOptions
+}
+
+export interface LayoutOptions {
+	/** The directory in which layouts are stored. */
+	directory?: string
 }
