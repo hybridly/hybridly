814336f250dcde2f19d0ac938ef436966ff1eaea
Enzo Innocenzi <enzo@innocenzi.dev>
2022-06-29 22:21:15 +0200

    feat(vite)!: remove `vite-plugin-laravel`

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,18 +1,15 @@
 import { PluginOption } from 'vite'
-import laravel from 'vite-plugin-laravel'
 import defineOptions from 'unplugin-vue-define-options/vite'
 import layout from './layout'
 import type { Options } from './types'
 
-/** Registers `vite-plugin-laravel` and `unplugin-vue-define-options`. */
+/** Registers `unplugin-vue-define-options`. */
 function plugin(options: Options = {}): PluginOption[] {
 	return [
-		laravel(options),
 		defineOptions(),
 		options.layout !== false && layout(options.layout),
 	]
 }
 
-export * from 'vite-plugin-laravel'
 export { layout, defineOptions, Options }
 export default plugin
