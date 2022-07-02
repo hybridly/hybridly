0c9ebd849ab3c44263cc30ffeae609c597f06aeb
Enzo Innocenzi <enzo@innocenzi.dev>
2022-07-03 01:45:46 +0200

    feat: support case conversion

---
+++ packages/laravel/src/Support/CaseConverter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Monolikit\Support;
+
+final class CaseConverter
+{
+    public function convert(array $payload, string $case): array
+    {
+        return $this->toCase($payload, $case);
+    }
+
+    private function toCase(array $payload, string $case): array
+    {
+        $result = [];
+
+        foreach ($payload as $key => $value) {
+            if (\is_array($value)) {
+                $value = $this->toCase($value, $case);
+            }
+
+            $key = (string) match ($case) {
+                'snake' => str()->snake($key),
+                'camel' => str()->camel($key),
+            };
+
+            $result[$key] = $value;
+        }
+
+        return $result;
+    }
+}
