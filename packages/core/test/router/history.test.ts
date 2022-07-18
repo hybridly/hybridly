8f9ba6c109ed9410fd126dbbe6d7a492a8d99946
Enzo Innocenzi <enzo@innocenzi.dev>
2022-07-18 23:05:09 +0200

    test: adapt to refactor

--- packages/core/test/router/history.test.ts
+++ packages/core/test/router/history.test.ts
@@ -1,6 +1,10 @@
-import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest'
 import { setHistoryState } from '../../src/router/history'
 import { fakeRouterContext, returnsArgs } from '../utils'
+
+beforeEach(() => {
+	fakeRouterContext()
+})
 
 describe('setHistoryState', () => {
 	beforeAll(() => {
@@ -11,19 +15,19 @@ describe('setHistoryState', () => {
 		vi.clearAllMocks()
 	})
 
-	it('pushes the given context into the history', () => {
+	it('pushes the context into the history', () => {
 		const spy = vi.spyOn(window.history, 'pushState').mockImplementation(returnsArgs)
 
-		setHistoryState(fakeRouterContext())
+		setHistoryState()
 
 		expect(spy).toHaveBeenCalledOnce()
 		expect(spy.mock.results).toMatchSnapshot('context')
 	})
 
-	it('replaces the given context in the current history state', () => {
+	it('replaces the context in the current history state', () => {
 		const spy = vi.spyOn(window.history, 'replaceState').mockImplementation(returnsArgs)
 
-		setHistoryState(fakeRouterContext(), { replace: true })
+		setHistoryState({ replace: true })
 
 		expect(spy).toHaveBeenCalledOnce()
 		expect(spy.mock.results).toMatchSnapshot('context')
