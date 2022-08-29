6ac9c8f35cd49beb25acae93dab9b9dcf16e1329
Enzo Innocenzi <enzo@innocenzi.dev>
2022-08-29 20:50:35 +0200

    refactor(progress): extract in own package

---
+++ packages/progress/build.config.ts
@@ -0,0 +1,16 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+	entries: [
+		'src/index',
+	],
+	clean: true,
+	declaration: true,
+	externals: [
+		'vite',
+		'debug',
+	],
+	rollup: {
+		emitCJS: true,
+	},
+})
