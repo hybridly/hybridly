b2c5a5e13109b33c8d17deaaa399142b61f34f10
Enzo Innocenzi <enzo@innocenzi.dev>
2022-09-15 21:23:21 +0200

    fix(middleware): take persistent array into account

--- packages/laravel/src/Http/Middleware.php
+++ packages/laravel/src/Http/Middleware.php
@@ -14,6 +14,12 @@ class Middleware
     use Concerns\SharesFlashNotifications;
 
     /**
+     * Marks the given properties as persisted, which means they will
+     * always be present, even in partial responses.
+     */
+    protected array $persistent = [];
+
+    /**
      * Handle the incoming request.
      */
     public function handle(Request $request, Closure $next): Response
@@ -24,6 +30,7 @@ class Middleware
 
         hybridly()->setRootView(fn () => $this->rootView($request));
         hybridly()->setVersion(fn () => $this->version($request));
+        hybridly()->persist($this->persistent);
 
         if (method_exists($this, 'share')) {
             hybridly()->share(app()->call([$this, 'share']));
