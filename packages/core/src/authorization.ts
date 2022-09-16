690ce974fa65401276210ddf991ab7407c71b52d
Enzo Innocenzi <enzo@innocenzi.dev>
2022-09-16 12:32:56 +0200

    refactor(core): improve `can` typings

--- packages/core/src/authorization.ts
+++ packages/core/src/authorization.ts
@@ -1,13 +1,15 @@
-export interface Authorizable {
-	authorization: {
-		[action: string]: boolean
-	}
+export interface Authorizable<Authorizations extends Record<string, boolean>> {
+	authorization: Authorizations
 }
 
 /**
  * Checks whether the given data has the authorization for the given action.
  * If the data object has no authorization definition corresponding to the given action, this method will return `false`.
  */
-export function can<Data extends Authorizable>(resource: Data, action: keyof Data['authorization']): boolean {
-	return resource.authorization?.[action as any] ?? false
+export function can<
+	Authorizations extends Record<string, boolean>,
+	Data extends Authorizable<Authorizations>,
+	Action extends keyof Data['authorization']
+>(resource: Data, action: Action) {
+	return resource.authorization?.[action] ?? false
 }
