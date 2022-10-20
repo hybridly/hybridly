245c6fd17e470256c8f697ce710804f099f9ad8b
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2022-10-20 16:50:33 +0330

    Add more tests

--- tests/src/Laravel/Testing/TestResponseMacrosTest.php
+++ tests/src/Laravel/Testing/TestResponseMacrosTest.php
@@ -3,7 +3,7 @@
 use Hybridly\Testing\Assertable;
 use Illuminate\Testing\TestResponse;
 
-it('can make hybrid assertions', function () {
+it('can assert Hybrid responses', function () {
     $response = makeMockRequest(hybridly('test'));
 
     $success = false;
@@ -15,6 +15,45 @@ it('can make hybrid assertions', function () {
     );
 
     expect($success)->toBeTrue();
+});
+
+it('can assert non-Hybrid responses', function () {
+    $response = makeMockRequest('hello-world');
+
+    $success = false;
+    $response->assertNotHybrid(
+        function (Assertable $page) use (&$success) {
+            expect($page)->toBeInstanceOf(Assertable::class);
+            $success = true;
+        },
+    );
+
+    expect($success)->toBeFalse();
+});
+
+it('can assert the given hybrid property exist', function () {
+    $response = makeMockRequest(hybridly('test', ['foo' => 'bar']));
+
+    $response->assertHasHybridProperty('foo');
+});
+
+it('can assert the given hybrid property is missing', function () {
+    $response = makeMockRequest(hybridly('test', ['foo' => 'bar']));
+
+    $response->assertMissingHybridProperty('owo');
+});
+
+it('can assert the property at the given path has the expected value', function () {
+    $response = makeMockRequest(hybridly('test', ['foo' => 'bar']));
+
+    $response->assertHybridProperty('foo', 'bar');
+});
+
+it('can assert the payload property at the given path has the expected value', function () {
+    $response = makeMockRequest(hybridly('test', ['foo' => 'bar']));
+
+    $response->assertHybridPayload('view.name', 'test');
+    $response->assertHybridPayload('view.properties', ['foo' => 'bar']);
 });
 
 it('preserves the ability to continue chaining laravel test response calls', function () {
