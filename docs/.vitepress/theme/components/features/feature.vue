d58ca75c7c6903e1ce482444e3f9021f17649fdc
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2022-10-23 13:45:37 +0330

    Improve features section

--- docs/.vitepress/theme/components/features/feature.vue
+++ docs/.vitepress/theme/components/features/feature.vue
@@ -14,31 +14,29 @@ defineProps<{
 <template>
 	<component
 		:is="feature.url ? 'a' : 'div'"
-		class="flex max-w-md flex-col gap-y-5 gap-x-8 rounded-3xl bg-[#FFF4FF] p-8 transition
-        duration-300 dark:bg-[#2a2b31] sm:flex-row
+		class="grid max-w-sm gap-3 rounded-3xl bg-[#FFF4FF] px-8 py-7 transition
+        duration-300 dark:bg-[#2a2b31]
         "
 		:href="feature.url"
 		:class="{
 			'hover:bg-[#FFEBFF] dark:hover:bg-[#32323c]': feature.url,
 		}"
 	>
-		<div class="grid shrink-0 place-items-center">
+		<div class="flex items-center justify-start gap-3">
 			<div
-				class="grid h-14 w-14 place-items-center
+				class="grid h-11 w-11 place-items-center
                 rounded-full bg-gradient-to-bl
                 from-violet-300 to-pink-300 text-white dark:from-[#5F3D82] dark:to-violet-900 dark:text-pink-400
                 "
 			>
 				<div v-html="feature.icon" />
 			</div>
-		</div>
-		<div class="space-y-4">
 			<div class="text-center text-lg font-bold dark:text-white sm:text-left">
 				{{ feature.title }}
 			</div>
-			<div class="min-h-[6rem] font-medium opacity-50">
-				{{ feature.description }}
-			</div>
+		</div>
+		<div class="min-h-[5rem] font-medium opacity-50">
+			{{ feature.description }}
 		</div>
 	</component>
 </template>
--- docs/.vitepress/theme/components/features/features-section.vue
+++ docs/.vitepress/theme/components/features/features-section.vue
@@ -12,66 +12,63 @@ const features: FeatureItem[] = [
 	{
 		title: 'FULL IDE Support',
 		description: `
-			Every object you interact in Vue will have auto completion and type check.
-			Typescript will basically prevent you from making errors.
+			If it's typable, it's typed. Hybridly supports typing paginators,
+			routes, authorizations, and global properties.
 		`,
-		icon: '<div class="i-fluent-code-24-filled h-9 w-9" />',
+		icon: '<div class="i-fluent-code-24-filled h-7 w-7" />',
 		url: '',
 	},
 	{
 		title: 'Powerful Forms',
 		description: `
-			With the form helper you don’t need to deal with handling responses
-			and errors like a traditional SPA. Everything is already handled for you!
+			No need to deal with handling responses and errors manually.
+			Everything is already handled for you!
 		`,
-		icon: '<div class="i-ph-magic-wand h-8 w-8" />" />',
+		icon: '<div class="i-ph-magic-wand h-6 w-6" />" />',
 		url: '/guide/forms',
 	},
 	{
-		title: 'Infinite Scrolling Support',
+		title: 'Infinite Scrolling',
 		description: `
 			Having the infinite scrolling support enables for so many possibities when
 			designing modern UX.
 		`,
-		icon: '<div class="i-ion-infinite-outline h-8 w-8" />" />',
+		icon: '<div class="i-ion-infinite-outline h-6 w-6" />" />',
 		url: '',
 	},
 	{
 		title: 'Typed Paginator',
 		description: `
-			You no longer need to remember how Laravel’s paginator object look like.
-			We provide first-party support for it so you can easily wrap your properties around it.
+			We provide first-party support for Laravel’s paginator so you can easily work with it's object.
 		`,
-		icon: '<div class="i-fluent-document-page-number-24-regular h-8 w-8" />" />',
+		icon: '<div class="i-fluent-document-page-number-24-regular h-6 w-6" />" />',
 		url: '',
 	},
 	{
 		title: 'Test Helpers',
 		description: `
-			To help you confidently test your Laravel application we have provided
-			fluent test helpers so your pages can easily be battle tested.
+			To help you confidently test your Laravel application we've provided
+			fluent test helpers.
 		`,
-		icon: '<div class="i-ph-flask-fill h-8 w-8" />" />',
+		icon: '<div class="i-ph-flask-fill h-6 w-6" />" />',
 		url: '/guide/testing',
 	},
 	{
-		title: 'Routes Auto-Completion',
+		title: 'Route Helper',
 		description: `
 			All your backend routes are accessible via the route helper method.
-			This allows you to not deal with urls manually and because they’re
-			typed you will not make mistakes.
+			No more visitng your route web.
 		`,
-		icon: '<div class="i-ph-plugs-connected h-8 w-8" />" />',
+		icon: '<div class="i-ph-plugs-connected h-6 w-6" />" />',
 		url: '/guide/routing',
 	},
 	{
 		title: 'Partial Reloads',
 		description: `
-			If you need to reload a specific set of data without doing a hard refresh,
-			you can easily do that by simply calling a method.
-			All your app state will be preserved!
+			You can refetch data without a hard refresh.
+			Plus, all your app state will be preserved!
 		`,
-		icon: '<div class="i-ion-reload h-8 w-8" />" />',
+		icon: '<div class="i-ion-reload h-6 w-6" />" />',
 		url: '/guide/partial-reloads',
 	},
 	{
@@ -80,14 +77,14 @@ const features: FeatureItem[] = [
 			If there are data that always need to be included in every response,
 			you can persist them.
 		`,
-		icon: '<div class="i-ph-database-duotone h-8 w-8" />" />',
+		icon: '<div class="i-ph-database-duotone h-6 w-6" />" />',
 		url: '/guide/persistent-properties',
 	},
 ]
 </script>
 
 <template>
-	<section class="mx-auto w-full max-w-screen-2xl px-3 pt-32">
+	<section class="mx-auto w-full max-w-screen-xl px-5 pt-32">
 		<div class="text-center">
 			<div class="text-5xl">
 				<span class="font-extrabold">
