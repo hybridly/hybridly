36c148ac2dd66f922d287bc2a870ad4d93c2cb06
Enzo Innocenzi <enzo@innocenzi.dev>
2022-11-28 11:58:55 +0100

    feat(resolve): support basic domain architecture

--- packages/vue/src/initialize.ts
+++ packages/vue/src/initialize.ts
@@ -128,6 +128,11 @@ function prepare(options: HybridlyOptions) {
  * Resolves a page component.
  */
 export async function resolvePageComponent(name: string, pages: Record<string, any>, defaultLayout?: any) {
+	if (name.includes(':')) {
+		const [domain, page] = name.split(':')
+		name = `domains.${domain}.pages.${page}`
+	}
+
 	const path = Object.keys(pages)
 		.sort((a, b) => a.length - b.length)
 		.find((path) => path.endsWith(`${name.replaceAll('.', '/')}.vue`))
