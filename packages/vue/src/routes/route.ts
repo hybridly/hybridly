9d4aef4fadc569f3741ac0f4a4aa2540ad11cd64
Enzo Innocenzi <enzo@innocenzi.dev>
2022-12-13 15:47:58 +0100

    chore: improve route error message

--- packages/vue/src/routes/route.ts
+++ packages/vue/src/routes/route.ts
@@ -17,7 +17,7 @@ export class Route {
 
 	static getDefinition(name: RouteName): RouteDefinition {
 		if (!state.routes.value) {
-			throw new Error('Routing is not initialized. Make sure the Vite plugin is enabled and that `virtual:hybridly/router` is imported.')
+			throw new Error('Routing is not initialized. Make sure the Vite plugin is enabled and that `virtual:hybridly/router` is imported and that `php artisan route:list` returns no error.')
 		}
 
 		const routes = state.routes.value as any
