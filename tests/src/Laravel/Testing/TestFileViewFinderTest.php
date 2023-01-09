a3cb1e03f0d7fd0faa377534c12630013dc4efe2
Enzo Innocenzi <enzo@innocenzi.dev>
2023-01-09 16:55:45 +0100

    test(testing): cover `TestFileViewFinder`

---
+++ tests/src/Laravel/Testing/TestFileViewFinderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Hybridly\Testing\TestFileViewFinder;
+use Illuminate\Support\Facades\File;
+
+it('finds normal pages in the views directory', function () {
+    /** @var TestFileViewFinder */
+    $finder = resolve('hybridly.testing.view_finder');
+    $pages_directory = "views/pages";
+    $page_path = "views/pages/my-page.vue";
+
+    File::makeDirectory(resource_path($pages_directory), recursive: true, force: true);
+    File::copy(__DIR__ . '/../../stubs/my-page.vue', resource_path($page_path));
+
+    expect($finder->find("my-page"))->toEndWith($page_path);
+});
+
+it('finds domain-based pages', function () {
+    /** @var TestFileViewFinder */
+    $finder = resolve('hybridly.testing.view_finder');
+    $domain = 'my-domain';
+    $pages_directory = "domains/{$domain}/pages";
+    $page_path = "domains/{$domain}/pages/my-page.vue";
+
+    File::makeDirectory(resource_path($pages_directory), recursive: true, force: true);
+    File::copy(__DIR__ . '/../../stubs/my-page.vue', resource_path($page_path));
+
+    expect($finder->find("{$domain}:my-page"))->toEndWith($page_path);
+});
