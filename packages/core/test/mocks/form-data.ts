2260766d72acd069e9683e8cfd986f031e2b94ee
Enzo Innocenzi <enzo@innocenzi.dev>
2023-01-11 20:04:14 +0100

    style: suppress warnings in mock file

--- packages/core/test/mocks/form-data.ts
+++ packages/core/test/mocks/form-data.ts
@@ -1,4 +1,7 @@
-export type FormDataEntryValue = File | string;
+/* eslint-disable unused-imports/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export type FormDataEntryValue = File | string
 
 export class FormData {
 	append(name: string, value: string | Blob, fileName?: string): void {}
@@ -6,12 +9,15 @@ export class FormData {
 	get(name: string): FormDataEntryValue | null {
 		return null
 	}
+
 	getAll(name: string): FormDataEntryValue[] {
 		return []
 	}
+
 	has(name: string): boolean {
 		return false
 	}
+
 	set(name: string, value: string | Blob, fileName?: string): void {}
 	forEach(callbackfn: (value: FormDataEntryValue, key: string, parent: FormData) => void, thisArg?: any): void {}
 }
