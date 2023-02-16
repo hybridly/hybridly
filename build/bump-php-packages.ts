8b43cba53551e59f517a8ce044fe1541a1b8aff9
Enzo Innocenzi <enzo@innocenzi.dev>
2023-02-16 15:57:23 +0100

    build: update process

--- build/bump-php-packages.ts
+++ build/bump-php-packages.ts
@@ -3,7 +3,10 @@ import * as ezSpawn from '@jsdevtools/ez-spawn'
 
 async function main() {
 	const { version } = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }))
-	await ezSpawn.async('composer', ['monorepo:release', '--', version], { stdio: 'inherit' })
+	await ezSpawn.async('composer', ['monorepo:merge'], { stdio: 'inherit' })
+	await ezSpawn.async('composer', ['monorepo:alias'], { stdio: 'inherit' })
+	await ezSpawn.async('composer', ['monorepo:bump', version], { stdio: 'inherit' })
+	await ezSpawn.async('composer', ['monorepo:validate'], { stdio: 'inherit' })
 }
 
 main()
