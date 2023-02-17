564b9a4f077b93dd2f05c34e668057429393d3a6
Johan van der Molen <hallo@johanvandermolen.nl>
2023-02-17 10:35:57 +0100

    Update authorization.ts

--- packages/core/src/authorization.ts
+++ packages/core/src/authorization.ts
@@ -6,10 +6,22 @@ export interface Authorizable<Authorizations extends Record<string, boolean>> {
  * Checks whether the given data has the authorization for the given action.
  * If the data object has no authorization definition corresponding to the given action, this method will return `false`.
  */
-export function can<
-	Authorizations extends Record<string, boolean>,
-	Data extends Authorizable<Authorizations>,
-	Action extends keyof Data['authorization']
->(resource: Data, action: Action) {
-	return resource.authorization?.[action] ?? false
+function can<
+    Authorizations extends Record<string, boolean>,
+    Data extends Authorizable<Authorizations>,
+    Action extends keyof Data['authorization']
+>(action: Action, resource: Data) {
+    if (resource === undefined) {
+        return useProperty('security.user').value?.authorization?.[action] ?? false
+    }
+
+    return resource.authorization?.[action] ?? false
+}
+
+function cannot<
+    Authorizations extends Record<string, boolean>,
+    Data extends Authorizable<Authorizations>,
+    Action extends keyof Data['authorization']
+>(action: Action, resource: Data) {
+    return ! can(action, resource)
 }
