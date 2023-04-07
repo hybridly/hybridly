4824791b6349afc86a9367106750290011d77c57
Enzo Innocenzi <enzo@innocenzi.dev>
2023-04-07 14:50:04 +0200

    feat(functions): move `is_hybrid` and `is_partial` to a namespace
    BREAKING CHANGE: You will need to import these functions in order to use them.

---
+++ packages/laravel/src/global_helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+use Hybridly\Hybridly;
+use Hybridly\View\Factory;
+use Illuminate\Contracts\Support\Arrayable;
+use Spatie\LaravelData\Contracts\DataObject;
+
+if (!function_exists('hybridly')) {
+    /**
+     * Gets the hybridly instance or returns a view.
+     *
+     * @phpstan-return ($component is string ? \Hybridly\View\Factory : \Hybridly\Hybridly)
+     */
+    function hybridly(string $component = null, array|Arrayable|DataObject $properties = []): Hybridly|Factory
+    {
+        /** @var Hybridly */
+        $hybridly = resolve(Hybridly::class);
+
+        if (!is_null($component)) {
+            return $hybridly->view($component, $properties);
+        }
+
+        return $hybridly;
+    }
+}
--- packages/laravel/src/helpers.php
+++ packages/laravel/src/helpers.php
@@ -1,12 +1,11 @@
 <?php
 
-use Hybridly\Hybridly;
-use Hybridly\View\Factory;
-use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Http\Request;
-use Spatie\LaravelData\Contracts\DataObject;
+namespace Hybridly;
 
-if (!function_exists('is_hybrid')) {
+use Hybridly\Support\Partial;
+use Illuminate\Http\Request;
+
+if (!\function_exists('Hybridly\is_hybrid')) {
     /**
      * Checks if the given request is hybrid.
      */
@@ -16,31 +15,12 @@ if (!function_exists('is_hybrid')) {
     }
 }
 
-if (!function_exists('is_partial')) {
+if (!\function_exists('Hybridly\is_partial')) {
     /**
      * Checks if the given request is a partial hybrid request.
      */
     function is_partial(Request $request = null): bool
     {
         return hybridly()->isPartial($request);
-    }
-}
-
-if (!function_exists('hybridly')) {
-    /**
-     * Gets the hybridly instance or returns a view.
-     *
-     * @phpstan-return ($component is string ? \Hybridly\View\Factory : \Hybridly\Hybridly)
-     */
-    function hybridly(string $component = null, array|Arrayable|DataObject $properties = []): Hybridly|Factory
-    {
-        /** @var Hybridly */
-        $hybridly = resolve(Hybridly::class);
-
-        if (!is_null($component)) {
-            return $hybridly->view($component, $properties);
-        }
-
-        return $hybridly;
     }
 }
