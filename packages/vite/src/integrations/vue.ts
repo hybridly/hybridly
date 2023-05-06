cb677fcb7719894fa836fa1decd01ddc964f748c
Enzo Innocenzi <enzo@innocenzi.dev>
2023-05-06 20:26:45 +0200

    chore: ignore `vue.script` type

--- packages/vite/src/integrations/vue.ts
+++ packages/vite/src/integrations/vue.ts
@@ -19,6 +19,7 @@ function getVueOptions(options: ViteOptions): VueOptions {
 				},
 			},
 			script: {
+				// @ts-ignore
 				globalTypeFiles: [
 					path.resolve('.hybridly/back-end.d.ts'),
 				],
