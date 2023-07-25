aa47e3b99f4c43103370953afa176c649d29cd98
Enzo Innocenzi <enzo@innocenzi.dev>
2023-07-25 23:03:19 +0200

    feat(vue): add `setProperty`

--- packages/vue/src/composables/property.ts
+++ packages/vue/src/composables/property.ts
@@ -1,6 +1,6 @@
 import type { ComputedRef } from 'vue'
-import { computed, readonly } from 'vue'
-import { getByPath } from '@clickbar/dot-diver'
+import { computed, readonly, toValue } from 'vue'
+import { getByPath, setByPath } from '@clickbar/dot-diver'
 import type { Path, PathValue, SearchableObject } from '@clickbar/dot-diver'
 import { state } from '../stores/state'
 import { toReactive } from '../utils'
@@ -22,4 +22,24 @@ export function useProperty<
 		: string,
 ): ComputedRef<ReturnType> {
 	return computed(() => getByPath(state.context.value?.view.properties as GlobalHybridlyProperties, path) as ReturnType)
+}
+
+/**
+ * Sets the property at the given path to the given value.
+ * Note: this helper is experimental and may change in the future.
+ */
+export function setProperty<
+	Override = never,
+	T extends SearchableObject = GlobalHybridlyProperties,
+	P extends Path<T> & string = Path<T> & string,
+	ValueType = [Override] extends [never] ? PathValue<T, P> : Override
+>(
+	path: [Override] extends [never] ? P : string,
+	value: ValueType,
+): void {
+	if (!state.context.value?.view.properties) {
+		return
+	}
+
+	setByPath(state.context.value.view.properties as GlobalHybridlyProperties, path, toValue(value as any))
 }
