154b97093674d9ed5a4813d9de69a5a6e9c5c445
Enzo Innocenzi <enzo@innocenzi.dev>
2023-08-29 09:29:53 +0200

    feat(refining): add `SelectFilter`

---
+++ packages/laravel/src/Refining/Filters/SelectFilter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Hybridly\Refining\Filters;
+
+use Hybridly\Refining\Concerns\SupportsRelationConstraints;
+use Hybridly\Refining\Contracts\Filter as FilterContract;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+
+class SelectFilter implements FilterContract
+{
+    use SupportsRelationConstraints;
+
+    private function __construct(
+        protected array $options,
+        protected string $operator,
+    ) {
+    }
+
+    public function getType(): string
+    {
+        return 'select';
+    }
+
+    public function __invoke(Builder $builder, mixed $value, string $property): void
+    {
+        $options = array_is_list($this->options)
+            ? $this->options
+            : array_keys($this->options);
+
+        if (!\in_array($value, $options, strict: false)) {
+            return;
+        }
+
+        $value = array_is_list($this->options)
+            ? $value
+            : $this->options[$value] ?? null;
+
+        $this->applyRelationConstraint(
+            builder: $builder,
+            property: $property,
+            callback: fn (Builder $builder, string $column) => $builder->where(
+                column: $builder->qualifyColumn($column),
+                operator: $this->operator,
+                value: $value,
+            ),
+        );
+    }
+
+    public static function make(string $property, array $options, string $operator = '=', ?string $alias = null): Filter
+    {
+        return (new Filter(
+            filter: new static($options, $operator),
+            property: $property,
+            alias: $alias,
+        ))->metadata([
+            'options' => $options,
+        ]);
+    }
+}
