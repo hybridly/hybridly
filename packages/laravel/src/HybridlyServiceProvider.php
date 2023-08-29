b2f21370461ddeda231057bfdec16d66f03acb13
Enzo Innocenzi <enzo@innocenzi.dev>
2023-08-29 09:45:03 +0200

    perf(laravel): lazy-load `about` data

--- packages/laravel/src/HybridlyServiceProvider.php
+++ packages/laravel/src/HybridlyServiceProvider.php
@@ -131,7 +131,7 @@ class HybridlyServiceProvider extends PackageServiceProvider
 
     protected function registerAbout(): void
     {
-        AboutCommand::add('hybridly', [
+        AboutCommand::add('hybridly', fn () => [
             'Version (composer)' => Version::getPrettyComposerVersion(),
             'Version (npm)' => Version::getPrettyNpmVersion(),
             'Eager view loading' => config('hybridly.architecture.eager_load_views') ? '<fg=yellow;options=bold>ENABLED</>' : 'OFF',
