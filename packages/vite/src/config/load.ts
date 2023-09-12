c34a49a4dff333d3251192b37f872b59b2386918
Enzo Innocenzi <enzo@innocenzi.dev>
2023-09-12 03:23:02 +0200

    refactor(vite): use `execSync` instead of `exec`

--- packages/vite/src/config/load.ts
+++ packages/vite/src/config/load.ts
@@ -1,14 +1,12 @@
-import { promisify } from 'node:util'
-import { exec } from 'node:child_process'
+import { execSync } from 'node:child_process'
 import type { DynamicConfiguration } from '@hybridly/core'
 import type { ViteOptions } from '../types'
-const shell = promisify(exec)
 
 export async function loadConfiguration(options: ViteOptions): Promise<DynamicConfiguration> {
 	try {
 		const php = options.php ?? process.env.PHP_EXECUTABLE_PATH ?? 'php'
-		const { stdout } = await shell(`${php} artisan hybridly:config`)
-		return JSON.parse(stdout)
+		const stdout = execSync(`${php} artisan hybridly:config`)
+		return JSON.parse(stdout.toString('utf-8'))
 	} catch (e) {
 		console.error('Could not load configuration from [php artisan].')
 		throw e
