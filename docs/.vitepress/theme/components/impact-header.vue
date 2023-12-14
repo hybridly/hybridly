99080db491da0a65d71157065ac923107d77faf9
Enzo Innocenzi <enzo@innocenzi.dev>
2023-12-14 01:59:09 +0100

    docs: improve upgrade guide styling

---
+++ docs/.vitepress/theme/components/impact-header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+	impact: 'low' | 'medium' | 'high'
+}>()
+</script>
+
+<template>
+	<span
+		class="VPBadge text-sm px-2 rounded py-1 ml-2 mt-0.5"
+		:class="{
+			'bg-red-100 dark:bg-red-400/10 text-red-900 dark:text-red-300': impact === 'high',
+			'bg-orange-100 dark:bg-orange-400/10 text-orange-900 dark:text-orange-300': impact === 'medium',
+			'bg-gray-100 dark:bg-gray-400/10 text-gray-700 dark:text-gray-300': impact === 'low',
+		}"
+		:title="`This is expected to have a ${impact} impact on your project`"
+		v-text="impact + ' impact'"
+	/>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,14 @@
-/* eslint-disable import/first */
-export { default } from 'vitepress/theme'
-
 import 'uno.css'
 import './styles/brand.css'
 import './styles/global.css'
 import '@fontsource-variable/rubik'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import impactHeader from './components/impact-header.vue'
+
+export default {
+	extends: DefaultTheme,
+	enhanceApp({ app }) {
+		app.component('ImpactHeader', impactHeader)
+	},
+} satisfies Theme
