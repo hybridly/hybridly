16d651b4c564cc1f219b9b07dedf7fb3bd6a80d8
Enzo Innocenzi <enzo@innocenzi.dev>
2024-01-24 18:34:17 +0100

    refactor: extract in trait

--- packages/laravel/src/Tables/Actions/InlineAction.php
+++ packages/laravel/src/Tables/Actions/InlineAction.php
@@ -2,25 +2,9 @@
 
 namespace Hybridly\Tables\Actions;
 
-use Closure;
-use Hybridly\Tables\Actions\DataTransferObjects\InlineActionData;
-use Illuminate\Database\Eloquent\Model;
+use Hybridly\Tables\Actions\Concerns\ResolvesModel;
 
 class InlineAction extends BaseAction
 {
-    protected Closure $modelResolver;
-
-    public function resolveModelUsing(Closure $resolver): static
-    {
-        $this->modelResolver = $resolver;
-
-        return $this;
-    }
-
-    public function resolveModel(string $modelClass, InlineActionData $data): Model
-    {
-        $resolver = $this->modelResolver ?? fn (string $modelClass, InlineActionData $data) => $modelClass::findOrFail($data->recordId);
-
-        return \call_user_func($resolver, $modelClass, $data);
-    }
+    use ResolvesModel;
 }
