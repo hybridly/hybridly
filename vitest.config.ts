b7663d3fc044a4528b2d9474fcdef42d6abe916e
Owen Conti <owen@springloaded.co>
2024-02-02 14:00:18 -0700

    refactor: Remove unused alias for @hybridly/vue

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,6 @@ export default defineConfig({
 			'hybridly': alias('./packages/hybridly/src/'),
 			'@hybridly/core': alias('./packages/core/src/'),
 			'@hybridly/vite': alias('./packages/vite/src/'),
-			'@hybridly/vue': alias('./packages/vue/src/'),
 		},
 	},
 	test: {
