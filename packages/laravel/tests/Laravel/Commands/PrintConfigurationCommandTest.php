2c04b0d672060d502118cfe4693ad9cfd9c55465
Enzo Innocenzi <enzo@innocenzi.dev>
2024-03-27 03:12:42 +0100

    test: cover `PrintConfigurationCommand`

---
+++ packages/laravel/tests/Laravel/Commands/PrintConfigurationCommandTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use function Pest\Laravel\artisan;
+
+test('configuration can be printed', function () {
+    artisan('hybridly:config')
+        ->assertOk()
+        ->expectsOutputToContain(<<<TXT
+        "architecture":{"root_directory":"resources","components_directory":"components","application_main_path":"resources\/application\/main.ts"},"components":{"eager":true,"layouts":[],"views":[],"components":[],"files":[]
+        TXT);
+});
+
+test('configuration subsets can be pretty-printed', function (string $path, string $output) {
+    artisan("hybridly:config --pretty {$path}")
+        ->assertOk()
+        ->expectsOutput($output);
+})->with([
+    'architecture' => ['architecture', <<<JSON
+    {
+        "root_directory": "resources",
+        "components_directory": "components",
+        "application_main_path": "resources/application/main.ts"
+    }
+    JSON],
+    'architecture.root_directory' => ['architecture.root_directory', <<<JSON
+    "resources"
+    JSON],
+]);
