c7a6f663afc77a9e065b8364f1494ff5d51b6601
Enzo Innocenzi <enzo@innocenzi.dev>
2025-06-24 18:11:04 +0200

    fix(architecture): load nested views in nested empty directories

--- packages/laravel/src/Architecture/LazyComponentsResolver.php
+++ packages/laravel/src/Architecture/LazyComponentsResolver.php
@@ -50,7 +50,7 @@ class LazyComponentsResolver implements ComponentsResolver
             namespace: $namespace,
             depth: $depth,
             filter: function (string $file, string $directory) use ($filter) {
-                if ($filter && ! $filter($file, $directory)) {
+                if (! is_dir("{$directory}/{$file}") && $filter && ! $filter($file, $directory)) {
                     return false;
                 }
 
