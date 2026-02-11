0df313dba78cd8a9f7a4aa139b43ea40a5bf9eef
Enzo Innocenzi <enzo@innocenzi.dev>
2026-02-11 17:02:54 +0100

    fix(laravel): resolve arrayable properties during view property transformation

--- packages/laravel/src/View/Factory.php
+++ packages/laravel/src/View/Factory.php
@@ -5,8 +5,8 @@ namespace Hybridly\View;
 use Hybridly\Contracts\HybridResponse;
 use Hybridly\Exceptions\MissingViewComponentException;
 use Hybridly\Hybridly;
+use Hybridly\Support\Arr as SupportArr;
 use Hybridly\Support\Header;
-use Hybridly\Support\Hybridable;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\JsonResponse;
@@ -167,15 +167,7 @@ class Factory implements HybridResponse
 
     protected function transformProperties(array|Arrayable|TransformableData $properties): array
     {
-        if ($properties instanceof Hybridable) {
-            $properties = $properties->toHybridArray();
-        }
-
-        if ($properties instanceof Arrayable || $properties instanceof TransformableData) {
-            $properties = $properties->toArray();
-        }
-
-        return $properties;
+        return SupportArr::resolveArrayableProperties($properties);
     }
 
     protected function renderDialog(Request $request, Payload $payload)
